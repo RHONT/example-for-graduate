59110633a0b5c6b9156127a4370a6034b98e6b96
RHONT <rg915i@yandex.ru>
2023-10-07 00:46:11 +0300

    внес данные application.properties

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,9 +1,11 @@
 package ru.skypro.homework.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.SetPasswordDto;
+import ru.skypro.homework.dto.UpdateUserDto;
 import ru.skypro.homework.dto.UserDto;
 
 @Slf4j
@@ -11,16 +13,30 @@ import ru.skypro.homework.dto.UserDto;
 @RequestMapping("users/")
 public class UserController {
 
-    @PostMapping("set_password")
-    public ResponseEntity<?> setPassword(@RequestBody SetPasswordDto setPasswordDto) {
-        log.info("User {} change password","SomeUser");
-        return ResponseEntity.ok().build();
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping(path ="set_password")
+    public SetPasswordDto setPassword(@RequestBody SetPasswordDto setPasswordDto) {
+        log.info("User {} change password","Id user");
+        return new SetPasswordDto();
     }
 
-    @GetMapping
-    public ResponseEntity<?> infoAboutAuthUser() {
-        return ResponseEntity.ok(new UserDto());
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(path = "me")
+    public UserDto infoAboutAuthUser() {
+        return new UserDto();
     }
 
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping(path = "me")
+    public UpdateUserDto updateUserDto(@RequestBody UpdateUserDto updateUserDto){
+        log.info("User {} updated data","Id user");
+        return new UpdateUserDto();
+    }
 
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping(path = "me/image")
+    public void updateUserDto(@RequestParam MultipartFile avatarUser){
+        log.info("User {} update avatar","Id user");
+        //add avatar
+    }
 }
