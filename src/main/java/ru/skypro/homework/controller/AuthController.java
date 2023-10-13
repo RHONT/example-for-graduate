1b1aa63cc319baa4f0a014255f72321720dc7d16
RHONT <rg915i@yandex.ru>
2023-10-14 00:29:32 +0300

    - spring security теперь через базу работает - Поправил RegisterDto, роль теперь приходит как String - Созданы сущности UserEntity, Role
    Протестировано через фронт.

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -4,13 +4,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.homework.dto.Login;
-import ru.skypro.homework.dto.Register;
-import ru.skypro.homework.service.AuthService;
+import ru.skypro.homework.dto.LoginDto;
+import ru.skypro.homework.dto.RegisterDto;
+import ru.skypro.homework.entities.Role;
+import ru.skypro.homework.entities.UserEntity;
+import ru.skypro.homework.repository.RoleRepository;
+import ru.skypro.homework.repository.UsersRepository;
+
+import java.util.Collections;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
@@ -18,23 +28,38 @@ import ru.skypro.homework.service.AuthService;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthenticationManager authManager;
+    private final UsersRepository usersRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Login login) {
-        if (authService.login(login.getUsername(), login.getPassword())) {
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
+        Authentication authentication= authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword()));
+        if (authentication.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        } else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
     }
 
+    //todo Место для маппера user
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody Register register) {
-        if (authService.register(register)) {
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        } else {
+    public ResponseEntity<?> register(@RequestBody RegisterDto registerDto) {
+        if (usersRepository.existsByUsername(registerDto.getUsername())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+        Role role=roleRepository.findByName(registerDto.getRole()).get();
+
+        UserEntity user=new UserEntity();
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+        user.setRoles(Collections.singletonList(role));
+
+        usersRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
