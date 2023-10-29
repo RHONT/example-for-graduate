19070cf204d2363dff0b00236b8f32e0bba6aea5
RHONT <rg915i@yandex.ru>
2023-10-29 16:13:23 +0300

    - Тесты заработали, надо клепать их дальше

--- src/test/java/ru/skypro/homework/TestUserController.java
+++ src/test/java/ru/skypro/homework/TestUserController.java
@@ -2,26 +2,13 @@ package ru.skypro.homework;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpResponse;
-import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
 import ru.skypro.homework.controller.AuthController;
 import ru.skypro.homework.controller.UserController;
 import ru.skypro.homework.dto.LoginDto;
@@ -29,12 +16,10 @@ import ru.skypro.homework.dto.RegisterDto;
 import ru.skypro.homework.dto.SetPasswordDto;
 import ru.skypro.homework.repository.RoleRepository;
 import ru.skypro.homework.repository.UsersRepository;
-
-import java.io.IOException;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @Slf4j
@@ -71,9 +56,8 @@ public class TestUserController {
     void init() {
         registerPath = "http://localhost:" + port + "/register";
         loginPath = "http://localhost:" + port + "/login";
-        setPasswordPath = "http://localhost:" + port + "/set_password";
+        setPasswordPath = "http://localhost:" + port + "/users/set_password";
     }
-
 
     @Test
     @Order(1)
@@ -99,59 +83,34 @@ public class TestUserController {
         if (usersRepository.existsByUsername(testName)) {
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         } else assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-
     }
 
     @Test
     void login(){
-        LoginDto loginDto=new LoginDto("user@gmail.com","123123123");
-        ResponseEntity<?> responseEntity = restTemplate.postForObject(loginPath, loginDto, ResponseEntity.class);
+        LoginDto loginDto=new LoginDto("enemy@gmail.com","123123123");
+        ResponseEntity<?> responseEntity = restTemplate.postForEntity(loginPath, loginDto, ResponseEntity.class);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-
     }
-
 
     @Test
     @Order(3)
-    @Transactional
-    @Rollback(value = false)
     void setPassword() {
-        Authentication authentication = authManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        "super_user@gmail.com",
-                        "123123123"));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-
         SetPasswordDto setPasswordDto = new SetPasswordDto();
-        setPasswordDto.setNewPassword("1");
-        setPasswordDto.setCurrentPassword("123123123");
+        setPasswordDto.setNewPassword("123");
+        setPasswordDto.setCurrentPassword("1");
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth("super_user@gmail.com", "123123123");
+        headers.setBasicAuth("super_user@gmail.com", "1");
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
         HttpEntity<SetPasswordDto> httpEntity = new HttpEntity<>(setPasswordDto, headers);
-
-        ClientHttpRequestFactory requestFactory = new
-                HttpComponentsClientHttpRequestFactory(HttpClients.createDefault());
-
-        RestTemplate restTemplate2 = new RestTemplate();
-        restTemplate2.setRequestFactory(requestFactory);
-        restTemplate2.setErrorHandler(new DefaultResponseErrorHandler() {
-            public boolean hasError(ClientHttpResponse response) throws IOException {
-                HttpStatus statusCode = response.getStatusCode();
-                return statusCode.series() == HttpStatus.Series.SERVER_ERROR;
-            }
-        });
-
         SetPasswordDto responseSetDto =
                 restTemplate.postForObject(setPasswordPath, httpEntity, SetPasswordDto.class);
 
         log.info(responseSetDto.toString());
 
-        assertThat(responseSetDto.getNewPassword()).isEqualTo("1");
+        assertNotNull(responseSetDto.getNewPassword());
     }
 
 }
