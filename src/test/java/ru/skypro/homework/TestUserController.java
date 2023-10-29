645590ecc097814544022ce8de328a403d950798
RHONT <rg915i@yandex.ru>
2023-10-29 21:15:14 +0300

    - UserController почти завершен

--- src/test/java/ru/skypro/homework/TestUserController.java
+++ src/test/java/ru/skypro/homework/TestUserController.java
@@ -11,12 +11,12 @@ import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import ru.skypro.homework.controller.AuthController;
 import ru.skypro.homework.controller.UserController;
-import ru.skypro.homework.dto.LoginDto;
-import ru.skypro.homework.dto.RegisterDto;
-import ru.skypro.homework.dto.SetPasswordDto;
+import ru.skypro.homework.dto.*;
 import ru.skypro.homework.repository.RoleRepository;
 import ru.skypro.homework.repository.UsersRepository;
+
 import java.util.Collections;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,12 +51,18 @@ public class TestUserController {
     String registerPath;
     String loginPath;
     String setPasswordPath;
+    String infoAboutUserdPath;
+
+    LoginDto testLoginDto;
 
     @BeforeEach
     void init() {
         registerPath = "http://localhost:" + port + "/register";
         loginPath = "http://localhost:" + port + "/login";
         setPasswordPath = "http://localhost:" + port + "/users/set_password";
+        infoAboutUserdPath = "http://localhost:" + port + "/users/me";
+
+        testLoginDto=new LoginDto("enemy@gmail.com", "123123123");
     }
 
     @Test
@@ -67,55 +73,96 @@ public class TestUserController {
 
     @Test
     @Order(2)
-    void fillDbFakeData() {
-        String testName = "super_user@gmail.com";
+    void registerFakeUser() {
+        String testName = "grand@gmail.com";
         RegisterDto registerDtoUser = RegisterDto.builder().
                 username(testName).
-                firstName("Евгений").
-                lastName("Белых").
+                firstName("Грант").
+                lastName("Просто Грант").
                 password("123123123").
-                role("USER").build();
+                role("ADMIN").build();
 
-        ResponseEntity<?> response =
-                restTemplate.
-                        postForEntity(registerPath, registerDtoUser, ResponseEntity.class);
-
-        if (usersRepository.existsByUsername(testName)) {
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-        } else assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        if (!usersRepository.existsByUsername(testName)) {
+            ResponseEntity<?> response =
+                    restTemplate.
+                            postForEntity(registerPath, registerDtoUser, ResponseEntity.class);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        }
     }
 
     @Test
-    void login(){
-        LoginDto loginDto=new LoginDto("enemy@gmail.com","123123123");
-        ResponseEntity<?> responseEntity = restTemplate.postForEntity(loginPath, loginDto, ResponseEntity.class);
+    void login() {
+        ResponseEntity<?> responseEntity = restTemplate.postForEntity(loginPath, testLoginDto, ResponseEntity.class);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    @Order(3)
     void setPassword() {
         SetPasswordDto setPasswordDto = new SetPasswordDto();
-        setPasswordDto.setNewPassword("1");
-        setPasswordDto.setCurrentPassword("1");
+        setPasswordDto.setNewPassword("123123123");
+        setPasswordDto.setCurrentPassword("123123123");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth("super_user@gmail.com", "1");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity<SetPasswordDto> httpEntity = new HttpEntity<>(setPasswordDto, headers);
         SetPasswordDto responseSetDto =
-                restTemplate.postForObject(setPasswordPath, httpEntity, SetPasswordDto.class);
-
-        log.info(responseSetDto.toString());
+                restTemplate.postForObject(setPasswordPath, getHttpWithAuthAndBody(setPasswordDto), SetPasswordDto.class);
 
         assertNotNull(responseSetDto.getNewPassword());
     }
 
+    /**
+     * Выводы:
+     * Если spring security выбрасывает UNAUTHORIZED, то нельзя делать проверку на ошибку через assertThrow
+     * Так как это не метод выбрасывает ее, а spring security. Поэтому нужно у сущности ответа проверять эту ошибку.
+     *
+     * Метод login() у меня не создавал сессию. Чтобы вызвать метод infoAboutUser(), мне нужно было подшить туда
+     * авторизацию
+     */
     @Test
-    void infoAboutUser(){
+    void infoAboutUser() {
+        ResponseEntity<UserDto> exchange1 =
+                restTemplate.exchange(infoAboutUserdPath, HttpMethod.GET, getHttpEmpty(), UserDto.class);
+        assertEquals(HttpStatus.UNAUTHORIZED,exchange1.getStatusCode());
 
+        ResponseEntity<UserDto> exchange =
+                restTemplate.exchange(infoAboutUserdPath, HttpMethod.GET, getHttpWithAuthAndNotBody(), UserDto.class);
+        assertNotNull(exchange.getBody().getId());
+    }
+
+    @Test
+    void updateInfoUser() {
+        UpdateUserDto updateUserDto=
+                UpdateUserDto.builder().firstName("ChangeTest").lastName("ChangeTest").phone("1111").build();
+
+        ResponseEntity<UpdateUserDto> exchange =
+                restTemplate.exchange
+                        (infoAboutUserdPath, HttpMethod.PATCH, getHttpWithAuthAndBody(updateUserDto), UpdateUserDto.class);
+
+        assertEquals(HttpStatus.OK, exchange.getStatusCode());
+        assertEquals("ChangeTest", Objects.requireNonNull(exchange.getBody()).getFirstName());
+    }
+
+    private <T>HttpEntity<?> getHttpWithAuthAndBody(T objectDto ){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("enemy@gmail.com", "123123123");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<T> httpEntity = new HttpEntity<>(objectDto,headers);
+        return httpEntity;
+    }
+
+    private HttpEntity<?>  getHttpEmpty(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return new HttpEntity<>(headers);
+    }
+
+    private HttpEntity<?>  getHttpWithAuthAndNotBody(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("enemy@gmail.com", "123123123");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return new HttpEntity<>(headers);
     }
 
 }
