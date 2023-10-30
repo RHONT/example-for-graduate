5fae6fe199e1432f0eeddff5db46abc0af0aceed
RHONT <rg915i@yandex.ru>
2023-10-30 22:19:49 +0300

    Тесты rest окончены

--- src/test/java/ru/skypro/homework/AdCommentIdRepo.java
+++ src/test/java/ru/skypro/homework/AdCommentIdRepo.java
@@ -3,6 +3,12 @@ package ru.skypro.homework;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * В тестах невозможно изменять переменный объявленные в классе тестов, они постоянно обнуляются.
+ * А хардком их не задашь, ведь id постоянно скачут.
+ * А id знать хочется другим тестам.
+ * Поэтому была создана обертка Map<>, чтобы служить транспортом данных id объявлений и комментариев в тестах
+ */
 public class AdCommentIdRepo {
 
     private static final Map<String,Integer> idRepositories = new HashMap<>();
